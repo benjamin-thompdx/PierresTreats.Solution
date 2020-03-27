5fa4d5e62788c4b22d68c30baacf69a60fe14d56
Benjamin Thom <jaminthom@gmail.com>
2020-03-27 11:09:09 -0700

    update PierresTreats/Models/PierresTreatsContext.cs to include IdentityDbContext<ApplicationUser>

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PierresTreats.Models
 {
-  public class PierresTreatsContext : DbContext
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
   {
     public virtual DbSet<Treat> Treats { get; set; }
     public DbSet<Flavor> Flavors { get; set; }
